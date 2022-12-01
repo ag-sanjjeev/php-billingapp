4cf22b5939874694c0090705291d7e4b9354d467
ag-sanjjeev <91313054+ag-sanjjeev@users.noreply.github.com>
2022-12-01 18:02:19 +0530

    Dashboard layout created

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php /* Header Layout */ require_once './layouts/header.php'; /* Header Layout */ ?>
 
+<?php /* DashBoard Layout */ require_once './layouts/dashboard.php'; /* DashBoard Layout */ ?>
 
 <?php /* Footer Layout */ require_once './layouts/footer.php'; /* Footer Layout */ ?>
---
+++ layouts/dashboard.php
@@ -0,0 +1,96 @@
+<!-- @@@@@@@@@@@@@@@ DASHBOARD @@@@@@@@@@@@@@@ -->
+<div class="dashboard-layout">
+
+<!-- @@@@@@@@@@@@@@@ SMALL GRAPH @@@@@@@@@@@@@@@ -->
+<div class="row small-graph-container m-0">
+	
+	<h3 class="heading my-2 text-primary">
+		<i class="bi bi-pie-chart-fill"></i>
+		Overview Graphs
+	</h3>	
+
+	<div class="col-md-3 my-3">
+		
+		<div class="card bg-success">
+			
+			<p>test</p>
+
+		</div>
+
+	</div>
+
+	<div class="col-md-3 my-3">
+		
+		<div class="card bg-warning">
+			
+			<p>test</p>
+
+		</div>
+
+	</div>
+
+	<div class="col-md-3 my-3">
+		
+		<div class="card bg-danger">
+			
+			<p>test</p>
+
+		</div>
+
+	</div>
+
+	<div class="col-md-3 my-3">
+		
+		<div class="card bg-secondary">
+			
+			<p>test</p>
+
+		</div>
+
+	</div>
+
+	<hr>
+
+</div>
+<!-- @@@@@@@@@@@@@@@ /SMALL GRAPH @@@@@@@@@@@@@@@ -->
+
+<!-- @@@@@@@@@@@@@@@ PRODUCT PRICE HISTORY GRAPH @@@@@@@@@@@@@@@ -->
+<div class="row price-history-graph-container m-0">
+	
+	<h3 class="heading my-2 text-primary">
+		<i class="bi bi-graph-up"></i>
+		Product Price History
+	</h3>
+
+	<div class="col my-3">
+		
+		<div class="card bg-warning">
+			<p>test</p>
+		</div>
+
+	</div>
+
+</div>
+<!-- @@@@@@@@@@@@@@@ /PRODUCT PRICE HISTORY GRAPH @@@@@@@@@@@@@@@ -->
+
+<!-- @@@@@@@@@@@@@@@ BILLS HISTORY GRAPH @@@@@@@@@@@@@@@ -->
+<div class="row bills-history-graph-container m-0">
+	
+	<h3 class="heading my-2 text-primary">
+		<i class="bi bi-bar-chart-line-fill"></i>
+		Bills History
+	</h3>
+
+	<div class="col my-3">
+		
+		<div class="card bg-warning">
+			<p>test</p>
+		</div>
+
+	</div>
+
+</div>
+<!-- @@@@@@@@@@@@@@@ /BILLS HISTORY GRAPH @@@@@@@@@@@@@@@ -->
+
+</div>
+<!-- @@@@@@@@@@@@@@@ /DASHBOARD @@@@@@@@@@@@@@@ -->
